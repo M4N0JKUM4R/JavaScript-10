5101fc051925f9b2ffd196ecdbfefb1d34522981
Manoj Kumar <manojkumar.foo@gmail.com>
2022-05-05 01:32:24 +0530

    Adblock detector completed

---
+++ Adblock detector/script.js
@@ -0,0 +1,33 @@
+// Initialize DOM elements
+
+let detectEl = document.querySelector("#detect-blocker");
+let wrapper = document.querySelector(".blocker-wrapper");
+let button = document.querySelector("button");
+
+// Inject ad manually for the adblocker to hide
+
+let adClasses = ["ad", "ads", "adsbox", "ad-placement", "doubleclick", "adplaceholder", "ad-badge"];
+
+adClasses.forEach(el => {
+    detectEl.classList.add(el);
+})
+
+
+let detectStyles = window.getComputedStyle(detectEl).getPropertyValue("display");
+
+button.addEventListener("click", () => {
+    wrapper.classList.remove("show");
+})
+
+
+
+// Check for adblock 
+
+const checkAdBlocker = () => {
+    if(!wrapper.classList.contains("show")) {
+        console.log("Checking")
+        detectStyles === "none" ? wrapper.classList.add("show") : wrapper.classList.remove("show");
+    }
+}
+
+setInterval(checkAdBlocker,500);
